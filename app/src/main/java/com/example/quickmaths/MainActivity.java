f89d7f5b6561fbff8aa7314b0601710111a703c2
Bastien DURAND <thekilleur.omax2016@gmail.com>
2024-06-14 17:58:31 +0200

    Fonction des boutons sur la page d'accueil

--- app/src/main/java/com/example/quickmaths/MainActivity.java
+++ app/src/main/java/com/example/quickmaths/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.quickmaths;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +12,55 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button btnPlay;
+    private Button btnScore;
+    private Button btnExit;
+    private Button btnInfo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        //Bouton Play
+        btnPlay = findViewById(R.id.playButton);
+
+        btnPlay.setOnClickListener(view -> {
+            //TODO Changer l'interface a affiché
+            Intent intent = new Intent(this,activity_info.class);
+            startActivity(intent);
+        });
+
+        //Bouton Score
+        btnScore = findViewById(R.id.scoreButton);
+
+        btnScore.setOnClickListener(view -> {
+            //TODO Changer l'interface a affiché
+            Intent intent = new Intent(this,activity_info.class);
+            startActivity(intent);
+        });
+
+        //Bouton Exit
+        btnExit = findViewById(R.id.quitButton);
+
+        btnExit.setOnClickListener(view -> {
+            finish();
+            System.exit(0);
+        });
+
+        //Bouton Info
+        btnInfo = findViewById(R.id.infoButton);
+
+        btnInfo.setOnClickListener(view -> {
+            Intent intent = new Intent(this,activity_info.class);
+            startActivity(intent);
+        });
     }
+
+
 }
